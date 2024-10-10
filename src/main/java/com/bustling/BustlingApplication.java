930611e1f6a2beca9e94c3ad7cf6d5ef35e7b41a
Yunseong Jeong <jyerd333@gmail.com>
2024-10-10 20:01:53 +0900

    Initial commit

---
+++ src/main/java/com/bustling/BustlingApplication.java
@@ -0,0 +1,13 @@
+package com.bustling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BustlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BustlingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bustling/BustlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bustling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BustlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
